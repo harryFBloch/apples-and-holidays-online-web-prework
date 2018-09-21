66338bcb15e4144c28bfcd84912c3235d780c688
Harry Bloch <harryfbloch@gmail.com>
2018-09-21 16:28:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each {|key, value|
-  puts "#{key.humanize}:"
+  puts "#{key.to_s.humanize}:"
   value.each {|holiday, supplyArary|
     puts "\t#{holiday}: #{supplyArary.join(", ")}"
   }
